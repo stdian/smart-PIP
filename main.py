5f9c6614c9ff3564fcacedb62435ea0e31c00a42
Dmitriy Stepanov <stdian@mail.ru>
2018-05-26 10:02:19 +0800

    Config text

--- main.py
+++ main.py
@@ -13,63 +13,71 @@ console = ['argparse', 'colorama', 'termcolor']
 other = ['botocore', 'conf', 'cryptography', 'psutil', 'pygments']
 
 libraries = [main, math, data, web, django, kivy, gui, databases, console, other]
-
+variables = []
 
 
 def setup():
 	pass
 
 
-
-
-
-
-
 def main():
+	global variables
+
 	root = Tk()
 
-	variables = []
 	for i in range(len(libraries)):
 		variables.append(IntVar())
 
 	root.title('Smart pip')
-
 	root.resizable(0, 0)
+	root.config(bg = '#C8C8C8')
 
 	name = Label(root, text="Smart pip")
+	name.config(font = ('Consolas', 20, 'bold'), width = 13, bg = '#C8C8C8')
 	name.pack()
 
-	checkbutton1 = Checkbutton(root, text="Main", variable=variables[0])
+	checkbutton1 = Checkbutton(root, text="     Main", variable=variables[0])
+	checkbutton1.config(font = ('Consolas', 15, 'bold'), bg = '#C8C8C8')
 	checkbutton1.pack()
 
-	checkbutton2 = Checkbutton(root, text="Math", variable=variables[1])
+	checkbutton2 = Checkbutton(root, text="     Math", variable=variables[1])
+	checkbutton2.config(font = ('Consolas', 15, 'bold'), height = 1, bg = '#C8C8C8')
 	checkbutton2.pack()
 
-	checkbutton3 = Checkbutton(root, text="Data", variable=variables[2])
+	checkbutton3 = Checkbutton(root, text="     Data", variable=variables[2])
+	checkbutton3.config(font = ('Consolas', 15, 'bold'), height = 1, bg = '#C8C8C8')
 	checkbutton3.pack()
 
-	checkbutton4 = Checkbutton(root, text="Web", variable=variables[3])
+	checkbutton4 = Checkbutton(root, text="      Web", variable=variables[3])
+	checkbutton4.config(font = ('Consolas', 15, 'bold'), height = 1, bg = '#C8C8C8')
 	checkbutton4.pack()
 
-	checkbutton5 = Checkbutton(root, text="Django", variable=variables[4])
+	checkbutton5 = Checkbutton(root, text="   Django", variable=variables[4])
+	checkbutton5.config(font = ('Consolas', 15, 'bold'), height = 1, bg = '#C8C8C8')
 	checkbutton5.pack()
 
-	checkbutton6 = Checkbutton(root, text="Kivy", variable=variables[5])
+	checkbutton6 = Checkbutton(root, text="     Kivy", variable=variables[5])
+	checkbutton6.config(font = ('Consolas', 15, 'bold'), height = 1, bg = '#C8C8C8')
 	checkbutton6.pack()
 
-	checkbutton7 = Checkbutton(root, text="GUI", variable=variables[6])
+	checkbutton7 = Checkbutton(root, text="      GUI", variable=variables[6])
+	checkbutton7.config(font = ('Consolas', 15, 'bold'), height = 1, bg = '#C8C8C8')
 	checkbutton7.pack()
 
 	checkbutton8 = Checkbutton(root, text="DataBases", variable=variables[7])
+	checkbutton8.config(font = ('Consolas', 15, 'bold'), height = 1, bg = '#C8C8C8')
 	checkbutton8.pack()
 
-	checkbutton9 = Checkbutton(root, text="Console", variable=variables[8])
+	checkbutton9 = Checkbutton(root, text="  Console", variable=variables[8])
+	checkbutton9.config(font = ('Consolas', 15, 'bold'), height = 1, bg = '#C8C8C8')
 	checkbutton9.pack()
 
-	checkbutton10 = Checkbutton(root, text="Other", variable=variables[9])
+	checkbutton10 = Checkbutton(root, text="    Other", variable=variables[9])
+	checkbutton10.config(font = ('Consolas', 15, 'bold'), height = 1, bg = '#C8C8C8')
 	checkbutton10.pack()
 
 	button = Button(root, text="Install", command=setup)
+	button.config(font = ('Consolas', 20, 'bold'), width = 13)
 	button.pack()
 
 	root.mainloop()
